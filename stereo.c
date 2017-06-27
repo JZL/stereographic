83f52f2de7bfd1a672edf70ccab036b862fd31e5
Jonah <jlangli1@swarthmore.edu>
2017-06-27 18:08:14 -0400

    Started working on projection onto xy plane

--- stereo.c
+++ stereo.c
@@ -5,24 +5,28 @@
 #include <math.h>
 #include "RayPolygon.h"
 
+//https://cocalc.com/projects/b0966444-8a50-4f2a-a0a7-5d5e6e261ea5/files/2017-06-21-221142.sagews
 void vectorCross(Vector a, Vector b, Vector *c);
-int vectorDot(Vector a, Vector b);
+double vectorDot(Vector a, Vector b);
 void pointSub(Point a, Point b, Vector *c);
-double findT(Point *verts, struct Polygon poly, struct Ray ray, int * i_s);
+double findT(Point *verts, struct Polygon poly, struct Ray ray, int * i_s, Vector N);
 void printVector(Vector v, char* name);
 int maxN(Vector N);
 int abs(int);
 void normalize(Vector);
 void distPoints(Point *a, Point *b);
-int v = 2;
+void project(Point *p, Vector N);
+int v = 0;
 
 int main(int argc, char **argv){
-    FILE* fp;
+    FILE *fp;
+    FILE *writeFile;
     char magicNumStr[2];
-    int height, width;
+    int yimg, ximg;
+    Vector N;
     //sed '2,$s/\s//g' simple.bpm |tr -d "\n" > test.smaller.pbm
     //and then go in and and new lines to the header 
-    fp = fopen("bw.pbm", "r");
+    fp = fopen("test.smaller.pbm", "r");
     /* fp = fopen("test.smaller.pbm", "r"); */
     if(fp == NULL){
         printf("Error reading file\n");
@@ -35,17 +39,17 @@ int main(int argc, char **argv){
         exit(2);
     }
 
-    fscanf(fp, "%i %i\n",&height, &width );
+    fscanf(fp, "%i %i\n",&yimg, &ximg );
     /* printf("%i %i\n", height, width); */
-    char *imgArr = malloc(sizeof(int)*height*(width));
-    fread(imgArr, sizeof(int)*(width), (height), fp);
+    char *imgArr = malloc(sizeof(int)*yimg*(ximg));
+    fread(imgArr, sizeof(int)*(ximg), (yimg), fp);
     /* printf("%c\n", imgArr[115*(width)+5]); */
     //arr[i*width+j]
     int maxOutSide = 500;
     int *outArr = malloc(sizeof(int)*maxOutSide*maxOutSide);
     for(int i = 0; i<maxOutSide;i++){
         for(int j = 0; j<maxOutSide;j++){
-            outArr[j*(maxOutSide)+i] = 2;
+            outArr[j*(maxOutSide)+i] = 0;
         }
     }
 
@@ -56,28 +60,40 @@ int main(int argc, char **argv){
     int i_s[2];
     //CHANGE POLY ATTR BELOW
     Point verts[3]= {
-        {0,0, 0},
+        {ximg, 0, 0},
+        {ximg, yimg, 0},
+        {ximg, 0, 100},
+    };
+    Point endPoint = {(int)ximg*5, (int)yimg*5, (int)ximg*.5};
+    /*
+    Point verts[3]= {
+        {,0, 0},
         {200, 0, 300},
         {200, 200, 300},
-        /* {-1, 500, 20} */
     };
+    */
 
     struct Polygon poly = {
         3,
         false,
         verts
     };
-    Point endPoint = {100,100,300};
     struct Ray ray;
 
+    Vector v0v1, v0v2;
+    pointSub(verts[0], verts[1], &v0v1);
+    pointSub(verts[0], verts[2], &v0v2);
+
+    vectorCross(v0v1, v0v2, &N);
 
     //everything in pixel width
     /* printf("x: %i, y: %i\n", width, height); */
-    for(int j = 0; j<width;j++){
-        for(int i = 0; i<height;i++){
-            if(imgArr[j*height+i] == '1'){
-                ray.O[0] = i;//x
-                ray.O[1] = j;//y
+    for(int i = gggg0; i<yimg;i++){
+        for(int j = 0; j<ximg;j++){
+            //todo
+            if(imgArr[j*ximg+i] == '1'){
+                ray.O[0] = j;//x
+                ray.O[1] = i;//y
                 ray.O[2] = 0;//z
 
                 /* endPoint[0] = i; */
@@ -93,7 +109,7 @@ int main(int argc, char **argv){
                     printVector(ray.D, "D  ");
                 }
 
-                t = findT(verts, poly, ray, i_s);
+                t = findT(verts, poly, ray, i_s, N);
                 if(!v)printf("t: %f\n", t);
                 if(t<=0){
                     //either error or intersection behind origin, reject
@@ -107,15 +123,16 @@ int main(int argc, char **argv){
                 intersects = intersect(&poly, &ray, t, i_s[0], i_s[1], v);
 
                 if(!v) printVector(ray.P, "rayPB");
-                distPoints(&ray.P, &(verts[0]));
+                //distPoints(&(verts[0]), &ray.P);
+                project(&ray.P, N);
                 if(!v) printVector(ray.P, "rayPA");
-                distPoints(&ray.P, &(verts[0]));
                 if(!v) printf("intersects? %d\n", intersects);
                 if(ray.P[0]< maxOutSide && ray.P[0]>=0 && ray.P[1]< maxOutSide && ray.P[1]>=0){
                     if(intersects == 1){
-                        outArr[(int)(ray.P[1])*(maxOutSide)+(int)(ray.P[0])] = 1;
+                        //outArr[(int)(ray.P[1])*(maxOutSide)+(int)(ray.P[0])] = 1;
+                        outArr[(int)round(ray.P[1])*(maxOutSide)+(int)round(ray.P[0])] = 2;
                     }else{
-                        outArr[(int)(ray.P[1])*(maxOutSide)+(int)(ray.P[0])] = 0;
+                        outArr[(int)round(ray.P[1])*(maxOutSide)+(int)round(ray.P[0])] = 1;
                     }
                 }
                 /* if(intersects == 1){ */
@@ -139,32 +156,30 @@ int main(int argc, char **argv){
         }
     }
     /* printf("\n\n\n"); */
-    if(v==2){
-        printf("P2\n%i %i\n2\n", maxOutSide, maxOutSide);
-        for(int i = 0; i<maxOutSide;i++){
-            for(int j = 0; j<maxOutSide;j++){
-                /* printf("i: %i, j: %i, maxOutSide: %i\n", i, j, maxOutSide); */
-                printf("%i ", outArr[i*(maxOutSide)+j]);
-                /* fflush(stdout); */
-            }
-            printf("\n");
-        }
+    writeFile = fopen("out.pgm", "w");
+    if(writeFile == NULL){
+        printf("couldn't write to file");
     }
+    fprintf(writeFile, "P2\n%i %i\n2\n", maxOutSide, maxOutSide);
+    for(int i = 0; i<maxOutSide;i++){
+        for(int j = 0; j<maxOutSide;j++){
+            /* printf("i: %i, j: %i, maxOutSide: %i\n", i, j, maxOutSide); */
+            //printf("%i ", outArr[i*(maxOutSide)+j]);
+            fprintf(writeFile, "%i ", outArr[i*(maxOutSide)+j]);
+            /* fflush(stdout); */
+        }
+        //printf("\n");
+    }
+    fclose(writeFile);
 
     free(imgArr);
     free(outArr);
     fclose(fp);
 
 }
-double findT(Point *verts, struct Polygon poly, struct Ray ray, int *i_s){
-    Vector v0v1, v0v2, N;
+double findT(Point *verts, struct Polygon poly, struct Ray ray, int *i_s, Vector N){
     double d, t, N_dot_D;
     int maxOfN;
-
-    pointSub(verts[0], verts[1], &v0v1);
-    pointSub(verts[0], verts[2], &v0v2);
-
-    vectorCross(v0v1, v0v2, &N);
     d = vectorDot(verts[0], N);
     d = -1*d;
 
@@ -185,13 +200,13 @@ double findT(Point *verts, struct Polygon poly, struct Ray ray, int *i_s){
     maxOfN = maxN(N);
     if(!v){
         printf("maxOfN: %d\n", maxOfN);
-        printf("N_S: %f, %f, %f\n", N[0], N[1], N[2]);
+        printf("N_S: %f, %f, %f\n", (N)[0], (N)[1], (N)[2]);
     }
-    if(N[0] == maxOfN){
+    if((N)[0] == maxOfN){
         if(!v)printf("0 is maxOfN\n");
         i_s[0] = 1;
         i_s[1] = 2;
-    }else if(N[1] == maxOfN){
+    }else if((N)[1] == maxOfN){
         if(!v)printf("1 is maxOfN\n");
         i_s[0] = 0;
         i_s[1] = 2;
@@ -214,7 +229,7 @@ void vectorCross(Vector a, Vector b, Vector *c){
     (*c)[2] = a[0]*b[1]-a[1]*b[0];
 }
 
-int vectorDot(Vector a, Vector b){
+double vectorDot(Vector a, Vector b){
     return a[0]*b[0]+a[1]*b[1]+a[2]*b[2];
 }
 
@@ -228,9 +243,9 @@ void pointSub(Point a, Point b, Vector *c){
 
 int maxN(Vector N){
     double absN0, absN1, absN2;
-    absN0 = abs(N[0]);
-    absN1 = abs(N[1]);
-    absN2 = abs(N[2]);
+    absN0 = fabs(N[0]);
+    absN1 = fabs(N[1]);
+    absN2 = fabs(N[2]);
     if(absN0 >= absN1 && absN0>=absN2){
         return N[0];
     }else if(absN1 >= absN2 && absN1>=absN2){
@@ -257,8 +272,17 @@ void normalize(Vector a){
 void distPoints(Point *a, Point *b){
     if(!v){printVector(*a, "a");}
     if(!v){printVector(*b, "b");}
-    (*a)[0] = (*b)[0] - (*a)[0]; 
-    (*a)[1] = (*b)[1] - (*a)[1]; 
-    (*a)[2] = (*b)[2] - (*a)[2]; 
+    (*b)[0] = (*b)[0] - (*a)[0]; 
+    (*b)[1] = (*b)[1] - (*a)[1]; 
+    (*b)[2] = (*b)[2] - (*a)[2]; 
     if(!v){printVector(*a, "a");}
+}
+void project(Point *P, Vector N){
+    //(x, y, z) - (x*nx + y*ny + z*nz) (nx, ny, nz)
+    double scalarProj = vectorDot(*P, N);
+    if(!v)printf("scalarProj: %d\n", scalarProj);
+    if(!v)printVector(N, "N");
+    (*P)[0] = (*P)[0]-scalarProj*N[0];
+    (*P)[1] = (*P)[1]-scalarProj*N[1];
+    (*P)[2] = (*P)[2]-scalarProj*N[2];
 }
